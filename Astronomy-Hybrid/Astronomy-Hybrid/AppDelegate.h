51435953ec550f352ea22e898bfcfa405bde5e09
Nar Kumar <njn.kumar@gmail.com>
2020-01-27 10:42:12 -0800

    Initial Commit

---
+++ Astronomy-Hybrid/Astronomy-Hybrid/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy-Hybrid
+//
+//  Created by Niranjan Kumar on 1/27/20.
+//  Copyright © 2020 Nar Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Astronomy-Hybrid/Astronomy-Hybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Astronomy-Hybrid
+//
+//  Created by Niranjan Kumar on 1/27/20.
+//  Copyright © 2020 Nar Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
